93e7862e413148f6a309e2c6d678356934194e3b
biQte <patrykgonet@gmail.com>
2022-09-26 20:59:45 +0200

    Update script to skip existing rows TODO: remove lines 59 and 60 after adding remaining carriers and connecting them in database

--- xmlscript.py
+++ xmlscript.py
@@ -1,3 +1,5 @@
+import re
+from time import sleep
 import urllib3
 import xmltodict
 import mysql.connector
@@ -18,60 +20,52 @@ else:
 
 row = 0
 
+cursor = conn.cursor()
+
 for item in items:
-    if item['TYP_KURIERA'] == '2B.PACZKOMAT-B' or item['TYP_KURIERA'] == '2C.PACZKOMAT-C' or item['TYP_KURIERA'] == '2A.PACZKOMAT-A':
-        continue
+    # if item['TYP_KURIERA'] == '2B.PACZKOMAT-B' or item['TYP_KURIERA'] == '2C.PACZKOMAT-C' or item['TYP_KURIERA'] == '2A.PACZKOMAT-A' or item['TYP_KURIERA] == '1. KOPERTA':
+    #     continue
     if item['EAN'] == '':
         continue
     product_id = "SELECT id_product FROM pr_product WHERE ean13 = %s;"
     ean13 = (item['EAN'])
-    cursor = conn.cursor()
     cursor.execute(product_id, (ean13,))
-    result_product_id = cursor.fetchall()
-    if result_product_id is None:
+    result_product_ids = cursor.fetchall()
+    if result_product_ids is None:
         continue
-    carrier_type = "SELECT pr_carrier.id_carrier_type FROM pr_carrier JOIN pr_carrier_type ON pr_carrier_type.id_carrier_type = pr_carrier.id_carrier_type WHERE pr_carrier_type.typ_kuriera = %s;"
-    carrier = (item['TYP_KURIERA'])
-    cursor.execute(carrier_type, (carrier,))
-    result_carrier_types = cursor.fetchall()
-    if result_carrier_types is None:
-        continue
-    for result_carrier_type in result_carrier_types:
-        if int((str(result_carrier_type))[1:len(str(result_carrier_type)) - 2]) == 7:
+    for result_product_id in result_product_ids:
+        carrier_type = "SELECT pr_carrier.id_carrier_type FROM pr_carrier JOIN pr_carrier_type ON pr_carrier_type.id_carrier_type = pr_carrier.id_carrier_type WHERE pr_carrier_type.typ_kuriera = %s;"
+        carrier = (item['TYP_KURIERA'])
+        cursor.execute(carrier_type, (carrier,))
+        result_carrier_types = cursor.fetchall()
+        current_product_carriers = "SELECT pr_carrier.id_carrier_type FROM pr_carrier_type JOIN pr_carrier ON pr_carrier_type.id_carrier_type = pr_carrier.id_carrier_type JOIN pr_product_carrier ON pr_carrier_type.id_carrier_type = pr_product_carrier.id_carrier_type WHERE pr_product_carrier.id_product = %s;"
+        cursor.execute(current_product_carriers, result_product_id)
+        current_carrier_types = cursor.fetchall()
+        # print(current_carrier_types)
+        result_product_id = str(result_product_id)[
+            1:len(str(result_product_id)) - 2]
+        if result_product_id == "[]":
             continue
-        if str(result_product_id) == "[]":
+        if result_carrier_types is None:
             continue
-        check_if_record_exist = "SELECT id_product FROM pr_product_carrier WHERE id_product = %s;"
-        values_for_check = (str(result_product_id)[
-                            2:len(str(result_product_id)) - 3],)
-        cursor.execute(check_if_record_exist, values_for_check)
-        exists = cursor.fetchall()
-        if exists == result_product_id:
-            print("Record exist")
-            check_if_record_needs_update = "SELECT id_carrier_type FROM pr_product_carrier WHERE id_product = %s;"
-            check_if_record_needs_update_values = (
-                str(result_product_id)[2:len(str(result_product_id)) - 3],)
-            cursor.execute(check_if_record_needs_update,
-                           check_if_record_needs_update_values)
-            needs_update = cursor.fetchall()
-            if int(str(needs_update)[2:len(str(needs_update)) - 3]) != int(str(result_carrier_type)[
-                    1:len(str(result_carrier_type)) - 2]):
-                print("and needs update")
-                update = "UPDATE pr_product_carrier SET id_carrier_type = %s WHERE id_product = %s;"
-                update_values = (str(result_carrier_type)[1:len(str(
-                    result_carrier_type)) - 2], str(result_product_id)[2:len(str(result_product_id)) - 3])
-                cursor.execute(update, update_values)
-                conn.commit()
-                print("Record updated")
-                continue
-            print("and doesn't need update")
-            continue
-        sql = "INSERT INTO pr_product_carrier (id_product,id_carrier_type,id_shop) VALUES (%s, %s, 1);"
-        values = (str(result_product_id)[
-                  2:len(str(result_product_id)) - 3], str(result_carrier_type)[1:len(str(result_carrier_type)) - 2])
-        cursor.execute(sql, values)
-        conn.commit()
-        row += 1
-print(row, "records inserted")
+        for result_carrier_type in result_carrier_types:
+            for current_carrier_type in current_carrier_types:
+                print(result_carrier_type)
+                print(current_carrier_type)
+                if int(str(result_carrier_type)[1:len(str(result_carrier_type)) - 2]) == int(str(current_carrier_type)[1:len(str(current_carrier_type)) - 2]):
+                    print("rekord skipped")
+                    continue
+                else:
+                    if int(str(result_carrier_type)[1:len(str(result_carrier_type)) - 2]) == 7:
+                        continue
+                    print(result_product_id)
+                    sql = "INSERT INTO pr_product_carrier (id_product,id_carrier_type,id_shop) VALUES (%s, %s, 1);"
+                    values = (result_product_id, str(result_carrier_type)
+                              [1:len(str(result_carrier_type)) - 2])
+                    cursor.execute(sql, values)
+                    conn.commit()
+                    print("Row inserted")
+            print(item['TYP_KURIERA'])
+print("Rows inserted")
 
 conn.disconnect()
