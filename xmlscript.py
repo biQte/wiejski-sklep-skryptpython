fb4c36af7097ea9cd4e2425d5d45b4ff672c7c7a
biQte <patrykgonet@gmail.com>
2022-10-13 14:34:03 +0200

    Add comments

--- xmlscript.py
+++ xmlscript.py
@@ -23,10 +23,9 @@ row = 0
 cursor = conn.cursor()
 
 for item in items:
-    # if item['TYP_KURIERA'] == '2B.PACZKOMAT-B' or item['TYP_KURIERA'] == '2C.PACZKOMAT-C' or item['TYP_KURIERA'] == '2A.PACZKOMAT-A' or item['TYP_KURIERA] == '1. KOPERTA':
-    #     continue
     if item['EAN'] == '':
         continue
+    # Search for product ID that is associated with specified EAN13 number
     product_id = "SELECT id_product FROM pr_product WHERE ean13 = %s;"
     ean13 = (item['EAN'])
     cursor.execute(product_id, (ean13,))
@@ -34,14 +33,16 @@ for item in items:
     if result_product_ids is None:
         continue
     for result_product_id in result_product_ids:
+        # Search for carrier IDs based on carrier name from XML
         carrier_type = "SELECT pr_carrier.id_carrier_type FROM pr_carrier JOIN pr_carrier_type ON pr_carrier_type.id_carrier_type = pr_carrier.id_carrier_type WHERE pr_carrier_type.typ_kuriera = %s;"
         carrier = (item['TYP_KURIERA'])
         cursor.execute(carrier_type, (carrier,))
         result_carrier_types = cursor.fetchall()
+        # Search for current carrier for specified product
         current_product_carriers = "SELECT pr_carrier.id_carrier_type FROM pr_carrier_type JOIN pr_carrier ON pr_carrier_type.id_carrier_type = pr_carrier.id_carrier_type JOIN pr_product_carrier ON pr_carrier_type.id_carrier_type = pr_product_carrier.id_carrier_type WHERE pr_product_carrier.id_product = %s;"
         cursor.execute(current_product_carriers, result_product_id)
         current_carrier_types = cursor.fetchall()
-        # print(current_carrier_types)
+        # Parsing data
         result_product_id = str(result_product_id)[
             1:len(str(result_product_id)) - 2]
         if result_product_id == "[]":
@@ -49,23 +50,25 @@ for item in items:
         if result_carrier_types is None:
             continue
         for result_carrier_type in result_carrier_types:
+            # Check if product already has that type of carrier to avoid repeating data
             for current_carrier_type in current_carrier_types:
-                print(result_carrier_type)
-                print(current_carrier_type)
                 if int(str(result_carrier_type)[1:len(str(result_carrier_type)) - 2]) == int(str(current_carrier_type)[1:len(str(current_carrier_type)) - 2]):
                     print("rekord skipped")
                     continue
                 else:
                     if int(str(result_carrier_type)[1:len(str(result_carrier_type)) - 2]) == 7:
                         continue
-                    print(result_product_id)
                     sql = "INSERT INTO pr_product_carrier (id_product,id_carrier_type,id_shop) VALUES (%s, %s, 1);"
                     values = (result_product_id, str(result_carrier_type)
                               [1:len(str(result_carrier_type)) - 2])
                     cursor.execute(sql, values)
                     conn.commit()
                     print("Row inserted")
-            print(item['TYP_KURIERA'])
-print("Rows inserted")
+                    row += 1
+        carriers_in_database_querry = "SELECT pr_carrier_type.typ_kuriera FROM pr_carrier_type JOIN pr_product_carrier ON pr_carrier_type.id_carrier_type = pr_product_carrier.id_carrier_type WHERE pr_product_carrier.id_product = %s;"
+        cursor.execute(carriers_in_database_querry, (result_product_id,))
+        carriers_in_database = cursor.fetchall()
+        carrier_names = item["TYP_KURIERA"]
+print(row + " rows inserted")
 
 conn.disconnect()
